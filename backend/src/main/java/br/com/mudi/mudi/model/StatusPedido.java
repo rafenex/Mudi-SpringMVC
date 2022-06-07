280f40d000587cb20bca9e7c11f82ddf9a3033b3
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-07 20:04:49 -0300

    Modelando o status, rota do status

---
+++ backend/src/main/java/br/com/mudi/mudi/model/StatusPedido.java
@@ -0,0 +1,6 @@
+package br.com.mudi.mudi.model;
+
+public enum StatusPedido {
+	AGUARDANDO,APROVADO,ENTREGUE;
+
+}
--- backend/src/main/java/br/com/mudi/mudi/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/mudi/mudi/repository/PedidoRepository.java
@@ -1,12 +1,17 @@
 package br.com.mudi.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.mudi.mudi.model.Pedido;
+import br.com.mudi.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 		
 	
 }
