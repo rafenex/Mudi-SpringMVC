616ba993b5e398c7a7419ab1af21242526122cae
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-14 20:34:38 -0300

    implementando Logout

--- backend/src/main/java/br/com/mudi/mudi/WebSecurityConfig.java
+++ backend/src/main/java/br/com/mudi/mudi/WebSecurityConfig.java
@@ -21,7 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		   .formLogin(form -> form
 		            .loginPage("/login")
 		            .permitAll()
-		        );
+		        )
+		   .logout(logout -> logout.logoutUrl("/logout"));		
 		
 	}
 	
