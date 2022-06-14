a5a31705658d5a40bcc590fdd31ac965768e06dd
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-14 19:22:05 -0300

    formulario de login

--- backend/src/main/java/br/com/mudi/mudi/WebSecurityConfig.java
+++ backend/src/main/java/br/com/mudi/mudi/WebSecurityConfig.java
@@ -18,7 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.anyRequest().authenticated()
 		.and()
-		.httpBasic();
+		   .formLogin(form -> form
+		            .loginPage("/login")
+		            .permitAll()
+		        );
+		
 	}
 	
 	@Bean
