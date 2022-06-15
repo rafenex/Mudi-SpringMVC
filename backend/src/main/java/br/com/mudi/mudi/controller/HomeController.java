dd387b6ab6b34db192eb0dfe78bda15ed3d76590
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-14 22:59:26 -0300

    ordenação e paginação

--- backend/src/main/java/br/com/mudi/mudi/controller/HomeController.java
+++ backend/src/main/java/br/com/mudi/mudi/controller/HomeController.java
@@ -1,10 +1,11 @@
 package br.com.mudi.mudi.controller;
 
-
 import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,27 +17,24 @@ import br.com.mudi.mudi.model.Pedido;
 import br.com.mudi.mudi.model.StatusPedido;
 import br.com.mudi.mudi.repository.PedidoRepository;
 
-	@Controller
-	@RequestMapping("/home")
-	public class HomeController {
-		
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@GetMapping
 	public String home(Model model, Principal principal) {
-			List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
-			model.addAttribute("pedidos",pedidos);
-			return "home";
-		}
-	
-	@ExceptionHandler(IllegalArgumentException.class)
-	public String onError() {
-		return "redirect:/home";
+		
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0,3, sort);
+
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
+		model.addAttribute("pedidos", pedidos);
+		return "home";
 	}
-	
-	}
-		
-		
-		
-		
+
+
+
+}
