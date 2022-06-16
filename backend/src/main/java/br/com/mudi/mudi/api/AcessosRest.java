1316b194cda5d683209ec72e57d8c80a7f44ba4c
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-16 18:22:36 -0300

    Interceptor para monitorar as requisições

---
+++ backend/src/main/java/br/com/mudi/mudi/api/AcessosRest.java
@@ -0,0 +1,22 @@
+package br.com.mudi.mudi.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mudi.mudi.interceptor.InterceptadorDeAcessos;
+import br.com.mudi.mudi.interceptor.InterceptadorDeAcessos.Acesso;
+
+@RequestMapping("acessos")
+@RestController
+public class AcessosRest {
+	
+	@GetMapping
+	public List<Acesso> getAcessos(){
+		return InterceptadorDeAcessos.acessos;
+		
+	}
+
+}
